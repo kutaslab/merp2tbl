bd17c3eb719d0b2a334ac1d9c1d01808c985cc48
Tom Urbach <turbach@ucsd.edu>
2018-05-12 16:49:15 -0700

    important commit. all tests pass, added data validation test tath merp2tbl vs. merp -d are identical when no soft errors and non-NA values are identical with soft errors.

--- merp2tbl/merp2tbl_test.py
+++ merp2tbl/merp2tbl_test.py
@@ -4,6 +4,8 @@ import glob
 import pprint as pp
 import pytest
 import pdb
+import re
+import subprocess
 
 import merp2tbl 
 
@@ -32,7 +34,38 @@ def test_load_merp_file():
         with pytest.raises(Exception):
             merp_cmds = merp2tbl.parse_merpfile(mcf)
 
-def test_run_merp():
+def test_validate_merp2tbl():
+    ''' this checks all non-NA data output matches merp output row for row '''
+
+    for mcf in good_mcfs + softerror_mcfs:
+        print('testing ', mcf)
+
+        # run merp2tbl
+        result = merp2tbl.run_merp(mcf)
+        merp2tbl_vals = []
+        for i,r in enumerate(result):
+            val = r['value_f']
+            if not val == 'NA':
+                val = float(val)
+            merp2tbl_vals.append(val)
+        
+        # run merp -d and slurp values 
+        proc_res = subprocess.run(['merp', '-d', mcf], 
+                                  stdout=subprocess.PIPE, 
+                                  stderr=subprocess.PIPE)
+
+        merp_vals = [float(v) for v in proc_res.stdout.decode('utf-8').split('\n') 
+                     if len(v.strip()) > 0]
+
+        # if no merp error, check all values, else just the non-NAs
+        if proc_res.stderr.decode('utf-8') == '':
+            assert merp_vals == merp2tbl_vals
+        else:
+            assert all([merp_vals[i] == merp2tbl_vals[i]
+                        for i,v in enumerate(merp2tbl_vals) if v is not 'NA'])
+            
+
+def test_merp2tbl_output_format():
     for mcf in good_mcfs + softerror_mcfs:
         result = merp2tbl.run_merp(mcf)
 
@@ -42,9 +75,9 @@ def test_run_merp():
             print('# ' + '-' * 40)
             merp2tbl.format_output(result, format=format)
             print()
-        
 
 def test_select_columns():
+    ''' test column slicer for subsetting output '''
     for mcf in good_mcfs + softerror_mcfs:
         result = merp2tbl.run_merp(mcf)
 
